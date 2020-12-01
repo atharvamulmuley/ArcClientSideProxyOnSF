df1580bc4bb684fd2c056b3b562e7456f12129b6
Atharva Mulmuley <atharvam@microsoft.com>
2020-12-01 15:04:51 +0530

    removed the replacing of service manifest step from helper.ps1

--- Auxillary/Helper.ps1
+++ Auxillary/Helper.ps1
@@ -1,5 +1,3 @@
-Remove-Item ..\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg\ServiceManifest.xml
-Copy-Item .\ServiceManifest.xml -Destination ..\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg
 New-Item -Path ..\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg -Name "Code2" -ItemType "directory"
 Copy-Item .\arcK8sProxy.exe -Destination ..\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg\Code2
 Copy-Item .\config.yml -Destination ..\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg\Code2
