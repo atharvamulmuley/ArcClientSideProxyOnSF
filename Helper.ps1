b774735aa9fd51e548dde25eb1bfa1ee5df4a524
Atharva Mulmuley <atharvam@microsoft.com>
2020-12-01 10:33:50 +0530

    added support for config file

--- Helper.ps1
+++ Helper.ps1
@@ -2,4 +2,4 @@ Remove-Item .\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg\ServiceManifest.xm
 Copy-Item .\ServiceManifest.xml -Destination .\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg
 New-Item -Path .\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg -Name "Code2" -ItemType "directory"
 Copy-Item .\arcK8sProxy.exe -Destination .\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg\Code2
-
+Copy-Item .\config.yml -Destination .\GuestProxyAPI\pkg\Debug\GuestProxyAPIServicePkg\Code2
