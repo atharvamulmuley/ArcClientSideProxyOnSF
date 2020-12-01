c2b1e4aa96e9d29232b9d56f95033d267b696086
Atharva Mulmuley <atharvam@microsoft.com>
2020-12-01 10:49:51 +0530

    moved all the additional files into auxillary folder

--- Auxillary/DeployApplication.ps1
+++ Auxillary/DeployApplication.ps1
@@ -1,4 +1,4 @@
 Connect-ServiceFabricCluster
-Copy-ServiceFabricApplicationPackage .\GuestProxyAPI\pkg\Debug -ApplicationPackagePathInImageStore MyApplication
+Copy-ServiceFabricApplicationPackage ..\GuestProxyAPI\pkg\Debug -ApplicationPackagePathInImageStore MyApplication
 Register-ServiceFabricApplicationType -ApplicationPathInImageStore MyApplication
 New-ServiceFabricApplication -ApplicationName fabric:/GuestProxyAPI -ApplicationTypeName GuestProxyAPIType -ApplicationTypeVersion 1.0.0
